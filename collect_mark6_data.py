bb73379c7371d0bf04c8a8a345066caf0bc35e9e
Katie Bouman <klbouman@gmail.com>
2016-04-01 00:42:36 +0000

    fixed bugs

--- collect_mark6_data.py
+++ collect_mark6_data.py
@@ -13,7 +13,6 @@ for r in range(rpt):
     # time.sleep(1)
     x0 = np.array(adc5g.get_snapshot(r2, 'r2dbe_snap_8bit_0_data'))
     x1 = np.array(adc5g.get_snapshot(r2, 'r2dbe_snap_8bit_1_data'))
-    print x0.shape
     
     if r > 0:
         x0full = np.column_stack((x0full, x0))
@@ -25,5 +24,5 @@ for r in range(rpt):
     print x0full.shape
 
 
-np.save('dataSamp_' + sys.argv[2] + '_x0full_.npy', x0full)
-np.save('dataSamp_' + sys.argv[2] + '_x1full_.npy', x1full)
+np.save('dataSamp_' + sys.argv[2] + '_x0full.npy', x0full)
+np.save('dataSamp_' + sys.argv[2] + '_x1full.npy', x1full)
