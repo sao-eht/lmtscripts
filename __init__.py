59a108840f0fb07f60f20bc9ff59a0d194cb0ee3
Lindy Blackburn <lindylam@gmail.com>
2016-04-02 02:53:22 +0000

    enable import as module

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+.. module:: lmtscripts
+    :platform: Unix
+    :synopsis: useful scripts for EHT observations at LMT
+
+.. moduleauthor:: Lindy Blackburn <lindylam@gmail.com>
+.. moduleauthor:: Katie Bouman <klbouman@gmail.com>
+
+"""
