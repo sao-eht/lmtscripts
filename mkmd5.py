2379625088101a92758b938471b51d600078451b
Lindy Blackburn <lindylam@gmail.com>
2017-02-27 14:11:00 -0500

    skip 20-byte files from false mk6 scans

--- mkmd5.py
+++ mkmd5.py
@@ -1,11 +1,14 @@
 # print md5sum table from first 8192 bytes in each file
 
+import os
 import sys
 import commands
 
 files = sys.argv[1:]
 
 for f in files:
+    if os.path.getsize(f) <= 20: # skip false recording from Mark6
+        continue
     out = commands.getstatusoutput('head -c 8192 %s | md5sum' % f)[1][:-3]
     print out + ' ' + f
 
