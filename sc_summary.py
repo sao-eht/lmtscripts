31c1810fb47fbf3b4874ec92f4ab9cf4c6880689
Lindy Blackburn <lindylam@gmail.com>
2017-02-28 00:32:25 -0500

    pass/fail stats

--- sc_summary.py
+++ sc_summary.py
@@ -9,6 +9,10 @@ files = sys.argv[1:]
 for f in files:
     if os.path.getsize(f) <= 20: # skip false recording from Mark6
         continue
-    out = commands.getstatusoutput('scan_check -v -c  exthdr= %s' % f)[1].strip().split('\n')[-2:]
-    print "%s %s %s" % (f, out[0][-18:], out[1].strip().split()[4])
+    out = commands.getstatusoutput('scan_check -v -c  exthdr= %s' % f)[1].strip().split('\n')
+    date = out[0].strip().split()[-1]
+    r2head = out[-2][-18:]
+    gpsns = out[-1].strip().split()[4]
+    passfail = out[1][12:]
+    print "%s %s %s %-8s %s" % (date, f, r2head, gpsns, passfail)
 
