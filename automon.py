2a717e57a89128d930da2903f6aeeba8e3ddce8f
Lindy Blackburn <lindylam@gmail.com>
2019-12-10 17:26:01 +0000

    include remaining 2018 edits

--- automon.py
+++ automon.py
@@ -23,8 +23,9 @@ else:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('schedule', nargs=nargs, help='xml schedule (from vex2xml)', default=lastxml)
-parser.add_argument('-a', '--alc', help='do alc before scan', action='store_true', default=False)
-parser.add_argument('-p', '--pre', help='time before scan to do alc.py', default=5.)
+parser.add_argument('-a', '--alc', help='do alc <N> seconds before scan start time (default 5)',
+                    nargs='?', const=5., type=float, default=None)
+parser.add_argument('-n', '--newlines', help='put each status report on new line', action='store_true', default=False)
 args = parser.parse_args()
 
 r2dbes = ['r2dbe1', 'r2dbe2', 'r2dbe3', 'r2dbe4']
@@ -32,15 +33,16 @@ alc = "/home/oper/bin/alc.py"
 
 tree = ET.parse(args.schedule)
 root = tree.getroot()
-wipe = 30 * '\b' + 30 * ' ' + 30 * '\b'
-
-predt = timedelta(seconds=float(args.pre))
+if args.newlines:
+    preline = '\n'
+else:
+    preline = 30 * '\b' + 30 * ' ' + 30 * '\b'
 
 def timersleep(until, line='waiting.. '):
     now = datetime.utcnow()
     if now > until:
         return False
-    sys.stdout.write(wipe)
+    sys.stdout.write(preline)
     while now < until:
         wait = (until - now).total_seconds()
         sys.stdout.write('\r%s%.0f ' % (line, wait))
@@ -49,8 +51,8 @@ def timersleep(until, line='waiting.. '):
         now = datetime.utcnow()
     return True
 
-def runalc(r2dbes, line='alc ..'):
-    sys.stdout.write('%s\r%s' % (wipe, line))
+def runalc(r2dbes, line='alc '):
+    sys.stdout.write('%s\r%s' % (preline, line))
     for r2 in r2dbes:
         out = check_output([alc, r2])
         (th0, th1) = (int(a[4:].replace(',','')) for a in out.split('\n')[2].split()[-2:])
@@ -65,8 +67,8 @@ for scan in root.findall("scan"):
     start    = datetime.strptime(scan.get('start_time'), '%Y%j%H%M%S')
     stop     = start + timedelta(seconds=duration)
 
-    if args.alc and timersleep(start - predt, line='[%s %s] alc in.. ' % (name, source)):
-        runalc(r2dbes, line='[%s %s] alc .. ' % (name, source))
+    if (args.alc is not None) and timersleep(start - timedelta(seconds=float(args.alc)), line='[%s %s] alc in.. ' % (name, source)):
+        runalc(r2dbes, line='[%s %s] alc ' % (name, source))
     timersleep(start, line='[%s %s] starts in.. ' % (name, source))
     timersleep(stop, line='[%s %s] recording.. ' % (name, source))
 
